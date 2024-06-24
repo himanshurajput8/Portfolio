6a485f9eac30fc05a16d98a624b7acd82c737ac9
himanshurajput8 <rajputhimanshu913@gmail.com>
2024-06-24 14:35:12 +0530

    fixed errorr

--- form.js
+++ form.js
@@ -10,12 +10,14 @@ const formTempelate = `
             </div>
         </div>
 
-        <form >
+        <form action="https://api.web3forms.com/submit" method="POST">
             <div class="form-right-div">
-                
+
+                     <input type="hidden" name="access_key" value="6a83d070-c2d5-4e89-bef9-1ddf7debc75a">
                     <input class="form-input" type="text" placeholder="Name">
                     <input class="form-input" type="email" placeholder="E-mail">
                     <textarea class="form-input" name="message" placeholder="Message"></textarea>
+                    <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
                     <input  class="form-input" type="submit" placeholder="Submit">
                 
             </div>
