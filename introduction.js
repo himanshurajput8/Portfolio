55232d6c0b9fe83f806e199143df036dd92f87d8
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-01-23 09:32:22 +0530

    add new project

--- introduction.js
+++ introduction.js
@@ -9,7 +9,7 @@ export function renderIntroduction(){
       <div class="intro-name">    
 
         <span>Hey! This is..</span>
-        <span>Himanshu Rajput</span>
+        <span> Himanshu Rajput</span>
         <span>Front-end Developer</span> 
         <span class="intro-details">
           <p>Hi, I'm Himanshu Rajput a graduate from AKTU (Lucknow). I have a strong passion for web development
