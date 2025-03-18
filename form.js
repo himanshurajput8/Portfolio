3d368ebebb9276637f8c77b7cd98013fd2a6e0e4
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-03-18 13:53:29 +0530

    update form

--- form.js
+++ form.js
@@ -15,7 +15,7 @@ const formTempelate = `
 
                      <input type="hidden" name="access_key" value="6a83d070-c2d5-4e89-bef9-1ddf7debc75a"> 
                     <input class="form-input" type="text" name="name" placeholder="Name" required>
-                    <input class="form-input" type="email" name="email" placeholder="E-mail" required>
+                    <input class="form-input" type="email" name="email" placeholder="E-mail" required>              
                     <textarea class="form-input" name="message" placeholder="Message" required></textarea> 
                     <input type="checkbox" name="botcheck" class="hidden" style="display: none;"> 
                     <input  class="form-input" type="submit" value="Submit">   
