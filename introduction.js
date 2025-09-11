6894063a16af1423396ebeb498625c2e8264c676
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-09-11 19:26:36 +0530

    fix ui

--- introduction.js
+++ introduction.js
@@ -13,13 +13,13 @@ export function renderIntroduction(){
         <span>Front-end Developer</span> 
         
         <span class="intro-details">
-          <p>Hi, I'm Himanshu Rajput a graduate from AKTU (Lucknow). I have a strong passion for web development
+          <p> Hi, I'm Himanshu Rajput a graduate from AKTU (Lucknow). I have a strong passion for web development
             and front-end technologies My mission is to craft outstanding web experience that leave a lasting 
             impression.</p>
 
           <strong><b>Skills Constellation</b></strong>
 
-          <p> Possess strong proficiency in front-end technologies including javascript, React.js, HTML, CSS,
+          <p>Possess strong proficiency in front-end technologies including javascript, React.js, HTML, CSS,
             These skills enable me to create captivating and user-friendly web interface</p>
 
           <p>Let's collaborate, code, and embark on a journey to create web experiences that are truly out of this
