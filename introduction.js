30cb5f0612c42c974f3846e42fd03aab787d8677
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-09-12 19:37:14 +0530

    change color

--- introduction.js
+++ introduction.js
@@ -8,9 +8,9 @@ export function renderIntroduction(){
     <div class="introduction">
       <div class="intro-name">    
 
-        <span>Hey! This is..</span>
-        <span>Himanshu Rajput</span>
-        <span>Front-end Developer</span> 
+        <span> Hey! This is..</span>
+        <span> Himanshu Rajput</span>
+        <span> Front-end Developer</span> 
         
         <span class="intro-details">
           <p> Hi, I'm Himanshu Rajput a graduate from AKTU (Lucknow). I have a strong passion for web development
